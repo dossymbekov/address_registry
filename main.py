67bad0342d16d1ac201406c72d515b7061509875
dossymbekov <berser91@gmail.com>
2020-05-19 14:32:25 +0600

    updated

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ print(r)
 
 import requests
 
-url = "https://data.egov.kz/api/v4/s_ats/data"
+url = "https://data.egov.kz/api/v4/s_ats/data?apiKey=4d5fbfd8e00047d6931ede2d301c3caa"
 
 headers = {
-  'Content-Type': 'application/x-www-form-urlencoded'
+  'Content-Type': 'application/json',
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 }
 
-params = {'apiKey':'4d5fbfd8e00047d6931ede2d301c3caa'}
-response = requests.request(method='POST', url=url, headers=headers, params=params, verify=False)
+response = requests.request(method='GET', url=url, headers=headers)
 
-print(response.text.encode('utf8'))
+print(response.json())
